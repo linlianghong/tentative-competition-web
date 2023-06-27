d1369ad32b6405196b6a9449df4f72376720e19b
Pluto Deng <mymail33333@163.com>
2023-06-28 02:04:44 +0800

    登陆页面

---
+++ src/layouts/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <n-layout
+    class="app-layout-content flex-1!" :content-style="{
+    }"
+  >
+    <n-layout-content class="flex-1!" :content-style="{ height: '100%' }">
+      <RouterView />
+    </n-layout-content>
+  </n-layout>
+</template>
+
+<style scoped>
+.app-layout-content {
+    @apply h-full!;
+  }
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div m="x-10vw y-3vh" p="4" bg="#fff" rounded="xl">
+    <div>
+      <img>
+    </div>
+    <div p="4">
+      <header>登陆</header>
+      <n-form>
+        <n-grid>
+          <n-form-item-gi span="24">
+            <n-input />
+          </n-form-item-gi>
+          <n-form-item-gi span="24">
+            <n-input />
+          </n-form-item-gi>
+          <n-grid-item span="24">
+            忘记密码
+          </n-grid-item>
+          <n-grid-item span="24">
+            <n-button>登录</n-button>
+          </n-grid-item>
+          <n-grid-item span="24">
+            <n-button>立即注册</n-button>
+          </n-grid-item>
+        </n-grid>
+      </n-form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<route lang="yaml">
+meta:
+  layout: login
+</route>
