84eaf67e814fac6b4fb561c115c100d693a6f67b
Pluto Deng <mymail33333@163.com>
2023-07-04 02:58:25 +0800

    更新组织

--- src/pages/index/components/OrgCard.vue
+++ src/pages/index/components/OrgCard.vue
@@ -11,7 +11,7 @@ defineProps<{
   <a :href="url" target="_blank" m="t-70px" rounded-4 bg="white opacity-70" shadow="~ md" lt-lg="mt-50px">
     <div flex="~ justify-center">
       <div bg="white opacity-70" p="2" class="-mt-50px" h-100px w-100px rounded="1/2" lt-lg="h-70px w-70px -mt-35px">
-        <img h-full w-full rounded="1/2" object="contain" :src="logo">
+        <img h-full w-full object="contain" :src="logo">
       </div>
     </div>
     <div text="center 5" m="t-4 b-6" lt-lg="text-3">
