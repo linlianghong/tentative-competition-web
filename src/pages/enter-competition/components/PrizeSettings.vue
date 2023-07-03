fd7fab3b9ae96b737703f65e8120debef788b42c
Tangwei <1371755866@qq.com>
2023-07-04 02:40:17 +0800

    换图

--- src/pages/enter-competition/components/PrizeSettings.vue
+++ src/pages/enter-competition/components/PrizeSettings.vue
@@ -6,12 +6,16 @@ import no1 from 'src/assets/enter-competition/ABUIABAEGAAg8-zRowYooNXR8wYw9QE4jA
 import no2 from 'src/assets/enter-competition/ABUIABAEGAAg8vzRowYo68iD7gEw2QE4_wE.png.webp'
 import no3 from 'src/assets/enter-competition/ABUIABAEGAAg8vzRowYomNPfsAcw2QE4_wE.png.webp'
 import no4 from 'src/assets/enter-competition/ABUIABAEGAAg8-zRowYogOikrwMw9QU4kgE!600x600.png.webp'
+import bigPrize from 'src/assets/enter-competition/bigPrize.png'
 </script>
 
 <template>
   <PageItem :bg="bg" title="奖项设置" :title-style="{ marginBottom: '4vw' }">
     <div h-full grid="~ cols-1 gap-4vw items-center lg:cols-2">
-      <div grid="~ cols-3 items-center gap-4" font="bold">
+      <div>
+        <img :src="bigPrize" class="w-full object-contain">
+      </div>
+      <!-- <div grid="~ cols-3 items-center gap-4" font="bold">
         <div grid="col-span-3">
           <img w-full object="cover" :src="no0">
         </div>
@@ -27,7 +31,7 @@ import no4 from 'src/assets/enter-competition/ABUIABAEGAAg8-zRowYogOikrwMw9QU4kg
         <div grid="col-span-3">
           <img w-full object="cover" :src="no4">
         </div>
-      </div>
+      </div> -->
       <div>
         <img :src="cup" class="w-full object-contain">
       </div>
